c7b8701f8b8f928a257e7b21801eecdc3e90920e
ChantellDavis <155310121+ChantellDavis@users.noreply.github.com>
2024-08-14 17:56:09 -0400

    removed city name from moment element name

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 function displayTimezone() {
 let losAngelesDateDataElement = document.querySelector("#los-Angeles-Date-Data");
 let losAngelesTimeDataElement = document.querySelector("#los-Angeles-Time-Data");
-let losAngelesTimeElement = moment();
+let TimeElement = moment();
 
-losAngelesDateDataElement.innerHTML = (losAngelesTimeElement.tz(`America_Los_Angeles`).format(`MMMM Do YYYY`));
-losAngelesTimeDataElement.innerHTML = (losAngelesTimeElement.tz(`America_Los_Angeles`).format(`h:mm:ss [<small>]A[</small>]`));
+losAngelesDateDataElement.innerHTML = (TimeElement.tz(`America_Los_Angeles`).format(`MMMM Do YYYY`));
+losAngelesTimeDataElement.innerHTML = (TimeElement.tz(`America_Los_Angeles`).format(`h:mm:ss [<small>]A[</small>]`));
 
 let SydneyDateDataElement = document.querySelector("#sydney-Date-Data");
 let SydneyTimeDataElement = document.querySelector("#sydney-Time-Data");
 
-let sydneyTimeElement = moment();
 
-SydneyDateDataElement.innerHTML  = (sydneyTimeElement.tz(`Australia/Sydney`).format('MMMM Do YYYY'));
-SydneyTimeDataElement.innerHTML = (sydneyTimeElement.tz(`Australia/Sydney`).format('h:mm:ss [<small>]A[</small>]'))
+
+SydneyDateDataElement.innerHTML  = (TimeElement.tz(`Australia/Sydney`).format('MMMM Do YYYY'));
+SydneyTimeDataElement.innerHTML = (TimeElement.tz(`Australia/Sydney`).format('h:mm:ss [<small>]A[</small>]'))
 
 let tokyoDateDataElement = document.querySelector("#tokyo-Date-Data");
 let tokyoTimeDataElement = document.querySelector("#tokyo-Time-Data");
 
-let tokyoTimeElement = moment();
 
- tokyoDateDataElement.innerHTML  = (tokyoTimeElement.tz(`Asia/Tokyo`).format('MMMM Do YYYY'));
- tokyoTimeDataElement.innerHTML = (tokyoTimeElement.tz(`Asia/Tokyo`).format('h:mm:ss [<small>]A[</small>]'));
+
+ tokyoDateDataElement.innerHTML  = (TimeElement.tz(`Asia/Tokyo`).format('MMMM Do YYYY'));
+ tokyoTimeDataElement.innerHTML = (TimeElement.tz(`Asia/Tokyo`).format('h:mm:ss [<small>]A[</small>]'));
 
 }
 
