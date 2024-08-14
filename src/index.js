332b8f588ccd165d8a23b49f6ad25ef75631dcf1
ChantellDavis <155310121+ChantellDavis@users.noreply.github.com>
2024-08-14 19:47:41 -0400

    added the functionality of the select element

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function displayTimezone() {
+let TimeElement = moment();  
 let losAngelesDateDataElement = document.querySelector("#los-Angeles-Date-Data");
 let losAngelesTimeDataElement = document.querySelector("#los-Angeles-Time-Data");
-let TimeElement = moment();
 
 losAngelesDateDataElement.innerHTML = (TimeElement.tz(`America_Los_Angeles`).format(`MMMM Do YYYY`));
 losAngelesTimeDataElement.innerHTML = (TimeElement.tz(`America_Los_Angeles`).format(`h:mm:ss [<small>]A[</small>]`));
@@ -9,23 +9,39 @@ losAngelesTimeDataElement.innerHTML = (TimeElement.tz(`America_Los_Angeles`).for
 let SydneyDateDataElement = document.querySelector("#sydney-Date-Data");
 let SydneyTimeDataElement = document.querySelector("#sydney-Time-Data");
 
-
-
 SydneyDateDataElement.innerHTML  = (TimeElement.tz(`Australia/Sydney`).format('MMMM Do YYYY'));
 SydneyTimeDataElement.innerHTML = (TimeElement.tz(`Australia/Sydney`).format('h:mm:ss [<small>]A[</small>]'))
 
 let tokyoDateDataElement = document.querySelector("#tokyo-Date-Data");
 let tokyoTimeDataElement = document.querySelector("#tokyo-Time-Data");
 
+tokyoDateDataElement.innerHTML  = (TimeElement.tz(`Asia/Tokyo`).format('MMMM Do YYYY'));
+tokyoTimeDataElement.innerHTML = (TimeElement.tz(`Asia/Tokyo`).format('h:mm:ss [<small>]A[</small>]'));
 
+};
 
- tokyoDateDataElement.innerHTML  = (TimeElement.tz(`Asia/Tokyo`).format('MMMM Do YYYY'));
- tokyoTimeDataElement.innerHTML = (TimeElement.tz(`Asia/Tokyo`).format('h:mm:ss [<small>]A[</small>]'));
-
+function updateCityTimeDateData(event) {
+    let cityTimeZone = (event.target.value);
+    let cityName = cityTimeZone.replace("_", " ").split(`/`)[1];
+let cityTime = moment().tz(cityTimeZone);
+let citiesElement = document.querySelector("#cities");
+citiesElement.innerHTML = ` <h2 class="cityLa">${cityName}</h2>
+        <div class="losAngelesDateData" >
+        ${cityTime.format('MMMM Do YYYY')}  
+          </div>
+          <div class="losAngelesTimeData">
+          ${cityTime.format('h:mm:ss [<small>]A[</small>]')}
+        </div> `;
 }
 
 displayTimezone();
 setInterval(displayTimezone);
+
+
+
+
+let cityTimeDateElement = document.querySelector("#city");
+cityTimeDateElement.addEventListener("change", updateCityTimeDateData);
 
 
 
